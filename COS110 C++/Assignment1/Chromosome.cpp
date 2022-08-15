017b811c5059426102807400189c98d6c4902fc9
Tim Tam <timothymdevilliers@gmail.com>
2022-08-15 12:00:59 +0200

    Progress
    Worked on Chromosome and GA classes

--- COS110 C++/Assignment1/Chromosome.cpp
+++ COS110 C++/Assignment1/Chromosome.cpp
@@ -1,22 +1,72 @@
 #include "Chromosome.h"
-
+#include <string>
 Chromosome::Chromosome(int numGenes, RandomGenerator* rand)
 {
-    this->numGenes = numGenes;
     bool* genes = new bool[numGenes];
-    for (int i = 0 ; i < numGenes; i++) 
+    if(rand == NULL)
     {
-        genes[i] = rand->randomBool();
+        for(int i = 0; i < numGenes; i++)
+        {
+            genes[i] = false;
+        }
     }
+    else
+    {
+        for(int i = 0; i < numGenes; i++)
+        {
+            genes[i] = rand->randomBool();
+        }
+    }
+    if(numGenes < 0)
+        this->numGenes = 0;
+    else
+        this->numGenes = numGenes;
 }
 Chromosome::Chromosome(Chromosome* chromosome)
 {
-    this->numGenes = chromosome->numGenes;
+    if(chromosome == NULL)
+    {
+        this->numGenes = 0;
+        bool* genes = new bool[0];
+    }
+    else
+    {
+        bool* genes = new bool[numGenes];
+        this->numGenes = chromosome->getNumGenes();
+
+        for(int i = 0; i < this->numGenes; i++)
+        {
+            this->genes[i] = chromosome->getGenes()[i];
+        }
+    } 
 }
 Chromosome::Chromosome(bool* genes, int numGenes)
 {
-    this->numGenes = numGenes;
-    this->genes = genes;
+    if(numGenes < 0)
+    {
+        this->numGenes = 0;
+        this->genes = new bool[0];
+    }
+    else
+    {
+        this->numGenes = numGenes;
+        bool* genes = new bool[numGenes];
+        if(genes == NULL)
+        {
+            for (int i = 0; i < numGenes; i++)
+            {
+                this->genes[i] = false;
+            }
+            
+        }
+        else
+        {
+            for(int i = 0; i < numGenes; i++)
+            {
+                this->genes[i] = genes[i];
+            }
+        }
+    }
 }
 Chromosome::~Chromosome()
 {
@@ -24,7 +74,11 @@ Chromosome::~Chromosome()
 }
 double Chromosome::fitness(FitnessFunction* fitnessFunction, Chromosome* chromosome, int numGenes)
 {
-    
+    if(fitnessFunction == NULL || numGenes <=0)
+    {
+        return 0;
+    }
+    return fitnessFunction->calculateFitness(chromosome, numGenes);
 }
 int Chromosome::getNumGenes()
 {
@@ -32,15 +86,57 @@ int Chromosome::getNumGenes()
 }
 Chromosome* Chromosome::crossOver(Chromosome* c2)
 {
-
+    if(c2 == NULL)
+    {
+        return this;
+    }
+    double crossOverPoint = floor(c2->getNumGenes()/2);
+    bool* nGenes = new bool[numGenes];
+    for (int i = 0; i < crossOverPoint; i++)
+    {
+        nGenes[i] = this->genes[i];
+    }
+    for (int i = crossOverPoint; i < numGenes; i++)
+    {
+        nGenes[i] = c2->genes[i];
+    }
+    Chromosome* c3 = new Chromosome(nGenes, numGenes);
+    delete [] nGenes;
+    return c3;
 }
 Chromosome* Chromosome::mutate()
 {
-
+    bool* nGenes = new bool[numGenes];
+    for (int i = 0; i < numGenes; i++)
+    {
+        if (genes[i] == true)
+        {
+            nGenes[i] = false;
+        }
+        else
+        {
+            nGenes[i] = true;
+        }
+    }
+    Chromosome* c3 = new Chromosome(nGenes, numGenes);
+    delete [] nGenes;
+    return c3;
 }
 std::string Chromosome::toString()
 {
-    
+    std::string out = "";
+    for (int i = 0; i < numGenes; i++)
+    {
+        if(genes[i] == true)
+        {
+            out + "1";
+        }
+        else
+        {
+            out + "0";
+        }
+    }
+    return out;
 }
 bool* Chromosome::getGenes()
 {
--- COS110 C++/Assignment1/GA.cpp
+++ COS110 C++/Assignment1/GA.cpp
@@ -2,47 +2,103 @@
 
 GA::GA(int populationSize, RandomGenerator* rand, int numGenes, int selectionSize)
 {
-
+    this->populationSize = populationSize;
+    this->selectionSize = selectionSize;
+    Chromosome** population = new Chromosome*[populationSize];
+    for (int i = 0; i < populationSize; i++)
+    {
+        population[i] = new Chromosome(numGenes, rand);
+    }
 }
 GA::GA(GA* geneticAlgorithm)
 {
-
+    
 }
 GA::~GA()
 {
-
+    delete [] population;
 }
 Chromosome** GA::selection(FitnessFunction* fitnessFunction)
 {
-
+    for (int i = 0; i < populationSize; i++)
+    {
+        for (int j = i + 1; j < populationSize; j++)
+        {
+            if(fitnessFunction->calculateFitness(population[i], population[i]->getNumGenes()) < fitnessFunction->calculateFitness(population[j], population[i]->getNumGenes()))
+            {
+                Chromosome* temp = population[i];
+                population[i] = population[j];
+                population[j] = temp;
+            }
+        }
+    }
 }
 Chromosome** GA::inverseSelection(FitnessFunction* fitnessFunction)
 {
-
+    for (int i = 0; i < populationSize; i++)
+    {
+        for (int j = i + 1; j < populationSize; j++)
+        {
+            if(fitnessFunction->calculateFitness(population[i], population[i]->getNumGenes()) >= fitnessFunction->calculateFitness(population[j], population[i]->getNumGenes()))
+            {
+                Chromosome* temp = population[i];
+                population[i] = population[j];
+                population[j] = temp;
+            }
+        }
+    }
 }
 Chromosome** GA::crossOver(Chromosome* c1, Chromosome* c2)
 {
-
+    Chromosome** c3 = new Chromosome*[2];
+    c3[0] = c1->crossOver(c2);
+    c3[1] = c2->crossOver(c1);
 }
 Chromosome* GA::mutate(Chromosome* c1)
 {
-
+    return c1->mutate();
 }
 double GA::calculateAvgAccuracy(FitnessFunction* fitnessFunction)
 {
-
+    double avgFitness = 0;
+    for(int i = 0; i < populationSize; i++)
+    {
+        avgFitness += fitnessFunction->calculateFitness(population[i], population[i]->getNumGenes());
+    }
+    avgFitness /= populationSize;
+    return avgFitness;
 }
 double GA::calculateStd(FitnessFunction* fitnessFunction)
 {
-
+    double avgFitness = calculateAvgAccuracy(fitnessFunction);
+    double std = 0;
+    for(int i = 0; i < populationSize; i++)
+    {
+        std += pow(fitnessFunction->calculateFitness(population[i], population[i]->getNumGenes()) - avgFitness, 2);
+    }
+    std = sqrt(std/populationSize);
+    return std;
 }
 double GA::calculateVariance()
 {
-
+    double uniqueChromosomes = 0;
+    for (int i = 0; i < populationSize; i++)
+    {
+        bool isCopy = false;
+        for (int j = 0; i < populationSize; i++)
+        {
+            if(population[i]->toString() == population[j]->toString())
+                isCopy = true;
+        }
+        if(isCopy)
+            uniqueChromosomes++;
+    }
+    return uniqueChromosomes/populationSize;
+    
 }
 void GA::setPopulation(Chromosome** p)
 {
-
+    
 }
 Chromosome** GA::run(FitnessFunction* fitnessFunction)
 {
