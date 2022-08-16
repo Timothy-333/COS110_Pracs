fbdbe2085e82d73c4ab65a9ccaff7971fb006965
Tim Tam <timothymdevilliers@gmail.com>
2022-08-16 17:53:31 +0200

    Progress
    Worked on GA class

--- COS110 C++/Assignment1/GA.cpp
+++ COS110 C++/Assignment1/GA.cpp
@@ -20,39 +20,52 @@ GA::~GA()
 }
 Chromosome** GA::selection(FitnessFunction* fitnessFunction)
 {
+    Chromosome** selection = new Chromosome*[populationSize];
+    for (int i = 0; i < populationSize; i++)
+    {
+        selection[i] = new Chromosome(population[i]);
+    }
     for (int i = 0; i < populationSize; i++)
     {
         for (int j = i + 1; j < populationSize; j++)
         {
-            if(fitnessFunction->calculateFitness(population[i], population[i]->getNumGenes()) < fitnessFunction->calculateFitness(population[j], population[i]->getNumGenes()))
+            if(fitnessFunction->calculateFitness(selection[i], selection[i]->getNumGenes()) < fitnessFunction->calculateFitness(selection[j], selection[i]->getNumGenes()))
             {
-                Chromosome* temp = population[i];
-                population[i] = population[j];
-                population[j] = temp;
+                Chromosome* temp = selection[i];
+                selection[i] = selection[j];
+                selection[j] = temp;
             }
         }
     }
+    return selection;
 }
 Chromosome** GA::inverseSelection(FitnessFunction* fitnessFunction)
 {
+    Chromosome** inverseSelection = new Chromosome*[populationSize];
+    for (int i = 0; i < populationSize; i++)
+    {
+        inverseSelection[i] = new Chromosome(population[i]);
+    }
     for (int i = 0; i < populationSize; i++)
     {
         for (int j = i + 1; j < populationSize; j++)
         {
-            if(fitnessFunction->calculateFitness(population[i], population[i]->getNumGenes()) >= fitnessFunction->calculateFitness(population[j], population[i]->getNumGenes()))
+            if(fitnessFunction->calculateFitness(inverseSelection[i], inverseSelection[i]->getNumGenes()) >= fitnessFunction->calculateFitness(inverseSelection[j], inverseSelection[i]->getNumGenes()))
             {
-                Chromosome* temp = population[i];
-                population[i] = population[j];
-                population[j] = temp;
+                Chromosome* temp = inverseSelection[i];
+                inverseSelection[i] = inverseSelection[j];
+                inverseSelection[j] = temp;
             }
         }
     }
+    return inverseSelection;
 }
 Chromosome** GA::crossOver(Chromosome* c1, Chromosome* c2)
 {
     Chromosome** c3 = new Chromosome*[2];
     c3[0] = c1->crossOver(c2);
     c3[1] = c2->crossOver(c1);
+    return c3;
 }
 Chromosome* GA::mutate(Chromosome* c1)
 {
@@ -94,17 +107,69 @@ double GA::calculateVariance()
             uniqueChromosomes++;
     }
     return uniqueChromosomes/populationSize;
-    
 }
 void GA::setPopulation(Chromosome** p)
 {
-    
+    for (int i = 0; i < populationSize; i++)
+    {
+        delete population[i];
+        population[i] = new Chromosome(p[i]);
+    }
 }
 Chromosome** GA::run(FitnessFunction* fitnessFunction)
 {
-
+    Chromosome** winners = selection(fitnessFunction);
+    Chromosome** losers = inverseSelection(fitnessFunction);
+    Chromosome** offsprings = new Chromosome*[3*selectionSize];
+    Chromosome** nChromosomes = new Chromosome*[2];
+    Chromosome** P = new Chromosome*[populationSize];
+    for(int i = 0; i < selectionSize*2; i++)
+    {
+        nChromosomes = crossOver(winners[i], winners[i+1]);
+        offsprings[i] = nChromosomes[0];
+        offsprings[i+1] = nChromosomes[1];
+    }
+    for (int i = 0; i < selectionSize; i++)
+    {
+        offsprings[i + 2*selectionSize] = winners[i+2*selectionSize]->mutate();
+    }
+    for (int i = 0; i < populationSize; i++)
+    {
+        P[i] = population[i];
+    }
+    
+    for (int i = 0; i < 3*selectionSize; i++)
+    {
+        Chromosome* dyingChromosome = new Chromosome(losers[i]);
+        for (int j = 0; j < populationSize; j++)
+        {
+            if(dyingChromosome->toString() == P[j]->toString())
+            {
+                P[j] = offsprings[i];
+            }
+        }
+    }
+    delete [] winners;
+    delete [] losers;
+    delete [] offsprings;
+    delete [] nChromosomes;
+    return P;
+    
 }
 double** GA::run(FitnessFunction* fitnessFunction, int numGenerations)
 {
-
+    double** results = new double*[numGenerations];
+    for (int i = 0; i < numGenerations; i++)
+    {
+        results[i] = new double[2];
+    }
+    for (int i = 0; i < numGenerations; i++)
+    {
+        Chromosome** P = run(fitnessFunction);
+        population = P;
+        results[i][0] = calculateAvgAccuracy(fitnessFunction);
+        results[i][1] = calculateStd(fitnessFunction);
+        results[i][2] = calculateVariance();
+    }
+    return results;
 }
--- COS110 C++/Assignment1/main.cpp
+++ COS110 C++/Assignment1/main.cpp
@@ -1,95 +1,95 @@
 #include <iostream>
 #include <ctime>
 
-// #include "Chromosome.h"
-// #include "FitnessFunction.h"
-// #include "GA.h"
-// #include "RandomGenerator.h"
+#include "Chromosome.h"
+#include "FitnessFunction.h"
+#include "GA.h"
+#include "RandomGenerator.h"
 
 using namespace std;
 
-// double avg(double** arr, int d, int size) {
-//     double sum = 0;
-//     for (int i = 0; i < size; i++) {
-//         sum += arr[i][d];
-//     }
-//     return sum / size;
-// }
+double avg(double** arr, int d, int size) {
+    double sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i][d];
+    }
+    return sum / size;
+}
 
-// void benchMark(int numGenerations, int popSize, int selectionSize, int numGenes, long seed, bool generationResults, bool timeResults) {
-//     RandomGenerator* randomGenerator = new RandomGenerator(seed);
-//     FitnessFunction* fitnessFunction = new FitnessFunction();
-//     GA* ga = new GA(popSize, randomGenerator, numGenes, selectionSize);
-//     clock_t startTime = clock();
-//     double** results = ga->run(fitnessFunction, numGenerations);
-//     clock_t endTime = clock();
-//     cout << "---" << endl;
-//     if (generationResults) {
-//         for (int i = 0; i < numGenerations; i++) {
-//             cout << "Gen " << i << endl;
-//             cout << "Avg accuracy: " << results[i][0] << endl;
-//             cout << "Std: " << results[i][1] << endl;
-//             cout << "Variance: " << results[i][2] << endl;
-//             cout << "---"<<endl;
-//         }
-//     }
+void benchMark(int numGenerations, int popSize, int selectionSize, int numGenes, long seed, bool generationResults, bool timeResults) {
+    RandomGenerator* randomGenerator = new RandomGenerator(seed);
+    FitnessFunction* fitnessFunction = new FitnessFunction();
+    GA* ga = new GA(popSize, randomGenerator, numGenes, selectionSize);
+    clock_t startTime = clock();
+    double** results = ga->run(fitnessFunction, numGenerations);
+    clock_t endTime = clock();
+    cout << "---" << endl;
+    if (generationResults) {
+        for (int i = 0; i < numGenerations; i++) {
+            cout << "Gen " << i << endl;
+            cout << "Avg accuracy: " << results[i][0] << endl;
+            cout << "Std: " << results[i][1] << endl;
+            cout << "Variance: " << results[i][2] << endl;
+            cout << "---"<<endl;
+        }
+    }
 
-//     cout << "Run summary: " << endl;
-//     cout << "Avg avg: " << avg(results, 0, numGenerations) << endl;
-//     cout << "Avg std: " << avg(results, 1, numGenerations) << endl;
-//     cout << "Avg var: " << avg(results, 2, numGenerations) << endl;
-//     if(timeResults)
-//         cout << "Execution time: " << difftime(endTime, startTime)/1000 << "ms" << endl;
+    cout << "Run summary: " << endl;
+    cout << "Avg avg: " << avg(results, 0, numGenerations) << endl;
+    cout << "Avg std: " << avg(results, 1, numGenerations) << endl;
+    cout << "Avg var: " << avg(results, 2, numGenerations) << endl;
+    if(timeResults)
+        cout << "Execution time: " << difftime(endTime, startTime)/1000 << "ms" << endl;
 
-//     for (int i = 0; i < numGenerations; i++) {
-//         delete[] results[i];
-//     }
-//     delete[] results;
-//     delete ga;
-//     delete fitnessFunction;
-//     delete randomGenerator;
-// }
+    for (int i = 0; i < numGenerations; i++) {
+        delete[] results[i];
+    }
+    delete[] results;
+    delete ga;
+    delete fitnessFunction;
+    delete randomGenerator;
+}
 
-// void studentMain() {
-//     Chromosome** p = new Chromosome * [10];
-//     for (int i = 0; i < 10; i++) {
-//         bool* arr = new bool[10];
-//         for (int j = 0; j < 10; j++) {
-//             arr[j] = j%2;
-//             if (j == i) {
-//                 arr[j] = false;
-//             }
-//         }
-//         p[i] = new Chromosome(arr, 10);
-//         delete[] arr;
-//     }
-//     RandomGenerator* rand = new RandomGenerator(12345);
-//     FitnessFunction* fitnessFunction = new FitnessFunction();
-//     GA* ga = new GA(10, rand, 10, 1);
-//     ga->setPopulation(p);
-//     for (int i = 0; i < 10; i++) {
-//         delete p[i];
-//     }
-//     delete[] p;
-//     cout << ga->calculateAvgAccuracy(fitnessFunction) << endl;
-//     cout << ga->calculateStd(fitnessFunction) << endl;
-//     cout << ga->calculateVariance() << endl;
-//     p = ga->run(fitnessFunction);
-//     ga->setPopulation(p);
-//     cout << ga->calculateAvgAccuracy(fitnessFunction) << endl;
-//     cout << ga->calculateStd(fitnessFunction) << endl;
-//     cout << ga->calculateVariance() << endl;
-//     for (int i = 0; i < 10; i++) {
-//         delete p[i];
-//     }
-//     delete[] p;
-//     delete ga;
-//     delete rand;
-//     delete fitnessFunction;
-//     benchMark(10, 10, 1, 10, 500, true, false);
-// }
+void studentMain() {
+    Chromosome** p = new Chromosome * [10];
+    for (int i = 0; i < 10; i++) {
+        bool* arr = new bool[10];
+        for (int j = 0; j < 10; j++) {
+            arr[j] = j%2;
+            if (j == i) {
+                arr[j] = false;
+            }
+        }
+        p[i] = new Chromosome(arr, 10);
+        delete[] arr;
+    }
+    RandomGenerator* rand = new RandomGenerator(12345);
+    FitnessFunction* fitnessFunction = new FitnessFunction();
+    GA* ga = new GA(10, rand, 10, 1);
+    ga->setPopulation(p);
+    for (int i = 0; i < 10; i++) {
+        delete p[i];
+    }
+    delete[] p;
+    cout << ga->calculateAvgAccuracy(fitnessFunction) << endl;
+    cout << ga->calculateStd(fitnessFunction) << endl;
+    cout << ga->calculateVariance() << endl;
+    p = ga->run(fitnessFunction);
+    ga->setPopulation(p);
+    cout << ga->calculateAvgAccuracy(fitnessFunction) << endl;
+    cout << ga->calculateStd(fitnessFunction) << endl;
+    cout << ga->calculateVariance() << endl;
+    for (int i = 0; i < 10; i++) {
+        delete p[i];
+    }
+    delete[] p;
+    delete ga;
+    delete rand;
+    delete fitnessFunction;
+    benchMark(10, 10, 1, 10, 500, true, false);
+}
 int main()
 {
-    //studentMain();
+    studentMain();
     return 0;
 }
